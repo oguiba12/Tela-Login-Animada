dd4c7eaccb46a0cdae301c26702691f6c37eb235
Guilherme Moreira <93737741+oguiba12@users.noreply.github.com>
2022-04-11 13:00:58 -0300

    Add files via upload

--- src/Pages/Welcome/index.js
+++ src/Pages/Welcome/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import reactDom from 'react-dom';
 import {
   View,
   Text,
@@ -8,13 +7,21 @@ import {
   TouchableOpacity, //Botão
 } from 'react-native';
 
+import * as Animatable from 'react-native-animatable';
+
+import { useNavigation } from '@react-navigation/native';
+
 
 export default function Welcome() {
+
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
 
       <View style={styles.containerLogo} >
-        <Image
+        <Animatable.Image
+          animation={'flipInY'}
           source={require('../../Assets/logo.png')}
           style={{ width: '100%' }}
           resizeMode='contain' // Faz a imagem ter 100% do tamanho dela
@@ -22,14 +29,14 @@ export default function Welcome() {
         />
       </View>
 
-      <View style={styles.containerForm} >
+      <Animatable.View delay={600} animation={'fadeInUp'} style={styles.containerForm} >
         <Text style={styles.title}>Monitore e organize seus gastos em qualquer lugar</Text>
         <Text style={styles.text} >Faça o login para começar</Text>
 
-        <TouchableOpacity style={styles.button} >
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('SignIn')}>
           <Text style={styles.buttonText} >Acessar</Text>
         </TouchableOpacity>
-      </View>
+      </Animatable.View>
 
     </View>
   )
@@ -63,11 +70,13 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginTop: 28,
     marginBottom: 12,
+    textAlign: 'center',
 
   },
 
   text: {
     color: '#A1A1A1',
+    textAlign: 'center',
   },
 
   button: {
@@ -87,5 +96,5 @@ const styles = StyleSheet.create({
     fontSize: 18,
     color: '#FFF',
     fontWeight: 'bold',
-  }
+  },
 }) 
